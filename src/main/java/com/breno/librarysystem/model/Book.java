527be2b919f799db05f3aad7e94be715ef6a42f7
brenomorp <brenomoreirapro@gmail.com>
2026-04-15 14:52:58 -0300

    feat: add Book model

---
+++ src/main/java/com/breno/librarysystem/model/Book.java
@@ -0,0 +1,71 @@
+package com.breno.librarysystem.model;
+
+import java.time.Year;
+
+public class Book {
+    private String title;
+    private String author;
+    private Year publicationYear;
+    private String isbn;
+    private boolean available = true;
+
+    public Book() {}
+
+    public Book(String title, String author, Year publicationYear, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Year getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Year publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", releaseYear=" + publicationYear +
+                ", isbn='" + isbn + '\'' +
+                ", availability=" + available +
+                '}';
+    }
+}
