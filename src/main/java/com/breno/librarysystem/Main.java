c9b9781094d5feacda3da39a4cd48bcf15b21751
brenomorp <brenomoreirapro@gmail.com>
2026-04-24 09:48:28 -0300

    feat: initialize application with Menu in Main

--- src/main/java/com/breno/librarysystem/Main.java
+++ src/main/java/com/breno/librarysystem/Main.java
@@ -1,7 +1,10 @@
 package com.breno.librarysystem;
 
+import com.breno.librarysystem.view.Menu;
+
 public class Main {
     static void main(String[] args) {
-
+        Menu menu = new Menu();
+        menu.showMenu();
     }
 }
